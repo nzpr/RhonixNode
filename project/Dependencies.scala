dcfec9f4928f02e3285ebaa49c402c09a38bc1d4
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-08-29 21:35:26 +0200

    Add scalacheck lib for blakeHash generator in main code

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -37,6 +37,7 @@ object Dependencies {
   val catsMtlLaws       = "org.typelevel"        %% "cats-mtl-laws"   % "1.3.0"
   val kalium            = "com.github.rchain"     % "kalium"          % "0.8.1"
   val lightningj        = "org.lightningj"        % "lightningj"      % "0.5.2-Beta"
+  val scalacheck        = "org.scalacheck"       %% "scalacheck"      % "1.14.1"
   val legacyLibs        = Seq(
     magnolia,
     guava,
@@ -57,6 +58,7 @@ object Dependencies {
     kalium,
     lightningj,
     shapeless,
+    scalacheck,
   )
 
   // Testing frameworks
