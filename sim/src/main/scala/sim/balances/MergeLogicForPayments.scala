7d5c7e774f6d0b9753547e84ae29072563fbf3c4
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-10-25 22:31:48 +0400

    Merge logic for accounts

--- sim/src/main/scala/sim/balances/MergeLogicForPayments.scala
+++ sim/src/main/scala/sim/balances/MergeLogicForPayments.scala
@@ -2,11 +2,12 @@ package sim.balances
 
 import cats.effect.kernel.Sync
 import cats.syntax.all.*
-import diagnostics.syntax.all.kamonSyntax
 import sdk.diag.Metrics
 import sdk.history.ByteArray32
 import sdk.syntax.all.{effectSyntax, mapSyntax}
-import sim.balances.data.{BalancesDeploy, BalancesState}
+import sim.balances.data.{Account, BalancesDeploy}
+
+import scala.math.Numeric.LongIsIntegral
 
 object MergeLogicForPayments {
 
@@ -17,20 +18,29 @@ object MergeLogicForPayments {
    *         None if negative value or long overflow detected. In this case, deploy should be rejected.
    * */
   def attemptCombine(
-    allBalances: BalancesState,
+    state: Map[Wallet, Account],
     deploy: BalancesDeploy,
-  ): Option[BalancesState] =
-    deploy.state.diffs.foldLeft(allBalances.some) { case (acc, (wallet, change)) =>
-      acc match {
-        case None      => acc
-        case Some(acc) =>
-          // Input args should ensure item is present in a map
-          val curV = allBalances.diffs.getUnsafe(wallet)
-          // Overflow should be fatal, since this is related to total supply
-          val newV = Math.addExact(curV, change)
-          Option.unless(newV < 0)(newV).as(new BalancesState(acc.diffs + (wallet -> newV)))
-      }
-    }
+  ): Option[Map[Wallet, Account]] = {
+    val curNonce = state.getUnsafe(deploy.deployer).nonce
+    val newNonce = Math.max(deploy.nonce, curNonce)
+    // deploys with nonces not greater then already folded should be discarded
+    if (newNonce == curNonce) none[Map[Wallet, Account]]
+    else
+      deploy.state.diffs
+        .foldLeft(state.some) { case (acc1, (wallet, change)) =>
+          acc1 match {
+            case None      => acc1
+            case Some(acc) =>
+              // Input args should ensure item is present in a map
+              val curV = state.getUnsafe(wallet).balance
+              // Overflow should be fatal, since this is related to total supply
+              val newV = Math.addExact(curV, change)
+              Option
+                .unless(newV < 0)(newV)
+                .as(acc.updated(wallet, acc.getUnsafe(wallet).copy(balance = newV, nonce = newNonce)))
+          }
+        }
+  }
 
   /**
    * Fold a sequence of items into initial state. Combination of an item with the state can fail.
@@ -50,27 +60,37 @@ object MergeLogicForPayments {
     baseState: ByteArray32,
     toFinalize: Set[BalancesDeploy],
     toMerge: Set[BalancesDeploy],
-  ): F[((BalancesState, Seq[BalancesDeploy]), (BalancesState, Seq[BalancesDeploy]))] = Sync[F].defer {
+  ): F[((Map[Wallet, Account], Seq[BalancesDeploy]), (Map[Wallet, Account], Seq[BalancesDeploy]))] = Sync[F].defer {
     val adjustedInFinal: Set[Wallet] = toFinalize.flatMap(_.state.diffs.keys)
     val adjustedInMerge: Set[Wallet] = toMerge.flatMap(_.state.diffs.keys)
     val adjustedAll: Set[Wallet]     = adjustedInFinal ++ adjustedInMerge
 
-    val readAllBalances = adjustedAll.toList
-      .traverse(k => reader.readBalance(baseState, k).map(_.getOrElse(0L)).map(k -> _))
+    val readAllAccounts = adjustedAll.toList
+      .traverse(k => reader.readAccount(baseState, k).map(_.getOrElse(Account.Default)).map(k -> _))
       .map(_.toMap)
 
-    readAllBalances
-      .flatMap { allInitValues =>
-        val initFinal        = new BalancesState(allInitValues.view.filterKeys(adjustedInFinal.contains).toMap)
-        val initAll          = new BalancesState(allInitValues)
-        val toFinalizeSorted = toFinalize.toList.sorted
-        val toMergeSorted    = toMerge.toList.sorted
+    readAllAccounts
+      .flatMap { initAll =>
+        val initFinal = initAll.view.filterKeys(adjustedInFinal.contains).toMap
+
+        // Deploys per each deployer should be ordered, deploys to fold should be ordered by deployer
+        def orderSet(x: Set[BalancesDeploy]) = {
+          implicit val deployOrdering: Ordering[BalancesDeploy] = Ordering.by(x => (x.nonce, x.id))
+          x.groupBy(_.deployer).view.mapValues(_.toList.sorted).toList.sortBy(_._1).flatMap(_._2)
+        }
+
+        val toFinalizeSorted = orderSet(toFinalize)
+        val toMergeSorted    = orderSet(toMerge)
 
         Sync[F]
           .delay(foldCollectFailures(initFinal, toFinalizeSorted, attemptCombine))
           .timedM("buildFinalState")
           .map { case (finChange, finRj) =>
-            val (mergeChange, provRj) = foldCollectFailures(initAll ++ finChange, toMergeSorted, attemptCombine)
+            val (mergeChange, provRj) = foldCollectFailures(
+              initAll ++ finChange,
+              toMergeSorted,
+              attemptCombine,
+            )
             (finChange, finRj) -> (mergeChange, provRj)
           }
       }
