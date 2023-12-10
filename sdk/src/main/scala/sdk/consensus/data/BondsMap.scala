199a7dbdb3f6b9031e6aa649d96fb49a6df9eedc
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-12-10 18:19:28 +0400

    Fix potential overflow

--- sdk/src/main/scala/sdk/consensus/data/BondsMap.scala
+++ sdk/src/main/scala/sdk/consensus/data/BondsMap.scala
@@ -7,7 +7,6 @@ final class BondsMap[A] private (val bonds: Map[A, Long]) extends AnyVal
 object BondsMap {
   def apply[A](bonds: Map[A, Long]): Try[BondsMap[A]] = Try {
     require(bonds.nonEmpty, "Bonds map cannot be empty!")
-    println(bonds)
     require(
       Try(bonds.values.foldLeft(0L) { case (a, b) =>
         require(b > 0, "Non positive stake!")
@@ -19,4 +18,12 @@ object BondsMap {
   }
 
   def unapply[A](bondsMap: BondsMap[A]): Option[Map[A, Long]] = Some(bondsMap.bonds)
+
+  def totalStake[A](bondsMap: BondsMap[A]): Long = bondsMap.bonds.values.sum
+
+  def isSuperMajority[A](bondsMap: BondsMap[A], target: Set[A]): Boolean = {
+    val totalStake  = BondsMap.totalStake(bondsMap)
+    val targetStake = bondsMap.bonds.view.filterKeys(target).values.sum
+    totalStake.toFloat / targetStake > 2f / 3
+  }
 }
--- sdk/src/test/scala/sdk/consensus/SelectQuorumOfSuperMajorityFromStateHashSpec.scala
+++ sdk/src/test/scala/sdk/consensus/SelectQuorumOfSuperMajorityFromStateHashSpec.scala
@@ -12,12 +12,10 @@ class SelectQuorumOfSuperMajorityFromStateHashSpec extends AsyncFlatSpec with Ma
 
   "next" should "be deterministic and produce quorums of super majority" in {
     forAll { (bonds: BondsMap[Int], stateHash: ByteArray) =>
-      val a      = SelectQuorumOfSuperMajorityFromStateHash(bonds).next(stateHash)
-      val b      = SelectQuorumOfSuperMajorityFromStateHash(bonds).next(stateHash)
+      val a = SelectQuorumOfSuperMajorityFromStateHash(bonds).next(stateHash)
+      val b = SelectQuorumOfSuperMajorityFromStateHash(bonds).next(stateHash)
       a shouldEqual b
-      val qStake = bonds.bonds.view.filterKeys(a.contains).values.sum
-      val tStake = bonds.bonds.values.sum
-      (qStake * 3) should be >= (tStake * 2)
+      BondsMap.isSuperMajority(bonds, a) shouldBe true
     }
   }
 }
