12db92baa16d01f921740a12bf0d65348373a35c
nutzipper <1746367+nzpr@users.noreply.github.com>
2024-02-03 14:24:50 +0400

    Move normalizer out of legacy package

--- legacy/src/main/scala/coop/rchain/rholang/normalizer2/env/VarContext.scala
+++
@@ -1,5 +0,0 @@
-package coop.rchain.rholang.normalizer2.env
-
-import coop.rchain.rholang.interpreter.compiler.SourcePosition
-
-final case class VarContext[T](index: Int, typ: T, sourcePosition: SourcePosition)
